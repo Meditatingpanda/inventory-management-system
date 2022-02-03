b6f41bb84e1a0672a93055256fdc73b47379d766
Gyana Ranjan panda <83230804+lonewolf2002@users.noreply.github.com>
2022-02-03 22:33:11 +0530

    complete

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -2,8 +2,10 @@ import React, { useEffect, useState } from "react";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { IconButton } from "@mui/material";
 let temp = [];
+
 function Orders() {
   const [orderHistory, setOrderHistory] = useState([]);
+
   useEffect(() => {
     setOrderHistory([...JSON.parse(localStorage.getItem("orderList") || "[]")]);
   }, []);
@@ -21,52 +23,61 @@ function Orders() {
       </div>
       <div>
         {orderHistory.map((key, id) => {
-          return (
-            <div
-              className="min-h-[10rem] bg-white mb-10 p-5 shadow-md "
-              key={id}
-            >
-              <div className="flex justify-around ">
-                <span>Customer Name: {key.customerName}</span>
-                <span>Contact Number: {key.phoneNum}</span>
-                <span>
-                  OrderId: <span className="text-red-400">{key.id}</span>{" "}
-                </span>
-                <span>
-                  <IconButton
-                    sx={{ color: "red" }}
-                    onClick={() => handleDelete(id)}
-                  >
-                    <DeleteIcon />
-                  </IconButton>
-                </span>
-              </div>
-              <div>
-                <div className="flex font-bold border-b-2 mb-4 border-red-200  justify-around">
-                  <span>Medicine Name</span>
-                  <span>Qty</span>
+          if (key.isSales === (localStorage.state === "admin" ? true : false))
+            return (
+              <div
+                className="min-h-[10rem] bg-white mb-10 p-5 shadow-md "
+                key={id}
+              >
+                <div className="flex justify-around ">
+                  <span>
+                    Customer Name:{" "}
+                    <span className="font-bold text-lg">
+                      {key.customerName}
+                    </span>
+                  </span>
+                  <span>
+                    Contact Number:
+                    <span className=" font-bold text-lg"> {key.phoneNum}</span>
+                  </span>
+                  <span>
+                    OrderId: <span className="text-red-400">{key.id}</span>{" "}
+                  </span>
+                  <span>
+                    <IconButton
+                      sx={{ color: "red" }}
+                      onClick={() => handleDelete(id)}
+                    >
+                      <DeleteIcon />
+                    </IconButton>
+                  </span>
                 </div>
-                {/* qty will be added here dynamically */}
                 <div>
-                  {key.cart.map((key, id) => {
-                    return (
-                      <div
-                        key={id}
-                        className="flex border-b-2 mb-4 border-red-200 text-gray-500  justify-around"
-                      >
-                        <span>{key.name}</span>
-                        <span>{key.qty}</span>
-                      </div>
-                    );
-                  })}
-                </div>
-                <div className="flex border-b-2 mb-4 border-red-300 font-bold  text-bold justify-around">
-                  <span>Total:-</span>
-                  <span>{key.totalAmount}</span>
+                  <div className="flex font-bold border-b-2 mb-4 border-red-200  justify-around">
+                    <span>Medicine Name</span>
+                    <span>Qty</span>
+                  </div>
+                  {/* qty will be added here dynamically */}
+                  <div>
+                    {key.cart.map((key, id) => {
+                      return (
+                        <div
+                          key={id}
+                          className="flex border-b-2 mb-4 border-red-200 text-gray-500  justify-around"
+                        >
+                          <span>{key.name}</span>
+                          <span>{key.qty}</span>
+                        </div>
+                      );
+                    })}
+                  </div>
+                  <div className="flex border-b-2 mb-4 border-red-300 font-bold  text-bold justify-around">
+                    <span>Total:-</span>
+                    <span>{key.totalAmount}</span>
+                  </div>
                 </div>
               </div>
-            </div>
-          );
+            );
         })}
       </div>
     </div>
